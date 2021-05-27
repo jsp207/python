116ebea5e6187b3e02497373cfa887a417e15645
jse4432@gmail.com <!@always33374>
2021-05-27 17:28:07 +0900

    업데이트

--- pybo/views.py
+++ pybo/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("HELLO WORLD PYTHON!!")
+    return HttpResponse("HELLO !! WELCOME TO JAY WORLD!!")
 # Create your views here.
