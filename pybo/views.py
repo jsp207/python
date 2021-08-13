c29f5491a60f17a179b2fcca8ed4eb56bbbf7fed
jse4432@gmail.com <!@always33374>
2021-08-13 17:20:00 +0900

    업데이트

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Question
 
 def index(request):
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list' : question_list}
+    return render(request, 'pybo/question_list.html', context)
+    """
     return HttpResponse("HELLO !! WELCOME TO JAY WORLD!!")
+    """
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = Question.objects.get(id=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
 # Create your views here.
